a8322594d0a0f9aa1deb97039b7cc4668ae9b5af
gzliew <guozhangliew@gmail.com>
2022-06-27 15:31:12 +0800

    [DOCKER-2] update pipeline

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -euo pipefail
+
+echo "start"
+
 if ! command -v container-structure-test &>/dev/null; then
 	echo "container-structure-test not found. Installing"
+
 	curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-arm64
+
+	echo "installing"
 
 	chmod +x container-structure-test-linux-arm64
 
@@ -12,6 +19,8 @@ if ! command -v container-structure-test &>/dev/null; then
 
 	mv container-structure-test-linux-arm64 $HOME/bin/container-structure-test
 fi
+
+echo "executing"
 
 container-structure-test test --image ${CONTAINER_NAME} --config $(pwd)/${CONTAINER_NAME}/unit-test.yml
 
