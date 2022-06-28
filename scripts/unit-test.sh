7463a09eb6acfc9aaadfbe945146d5f4d56b6e12
gzliew <43195293+GZLiew@users.noreply.github.com>
2022-06-28 11:01:23 +0800

    [DOCKER-2] add pipeline for unit-test (#4)

---
+++ scripts/unit-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if ! command -v container-structure-test &>/dev/null; then
+	echo "container-structure-test not found. Installing"
+
+	curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-arm64
+
+	chmod +x container-structure-test-linux-arm64
+
+	mkdir -p $HOME/bin
+
+	export PATH=$PATH:$HOME/bin
+
+	mv container-structure-test-linux-arm64 $HOME/bin/container-structure-test
+fi
+
+container-structure-test test --image ${CONTAINER_NAME} --config $(pwd)/${CONTAINER_NAME}/unit-test.yml
+
